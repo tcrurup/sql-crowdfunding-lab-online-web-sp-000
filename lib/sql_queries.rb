a40d71e770082476aef1e4f7698e6d8291d67b28
Anthony Rurup <tcrurup@gmail.com>
2019-05-15 20:00:51 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -15,7 +15,7 @@ GROUP BY projects.title"
 end
 
 def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_name
-"SELECT users.name, users.age, pledges.amount
+"SELECT users.name, users.age, SUM(pledges.amount)
 FROM pledges
 LEFT JOIN users
 ON pledges.user_id = users.id
