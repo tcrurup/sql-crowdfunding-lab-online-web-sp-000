f2a506e3c607386e25bb4543922efda4438d5ec9
Anthony Rurup <tcrurup@gmail.com>
2019-05-15 20:09:51 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -24,7 +24,7 @@ ORDER BY users.name"
 end
 
 def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_funding_goal
-"SELECT projects.title, 
+"SELECT projects.title, projects.funding_goal - SUM(pledges.)
 FROM projects
 INNER JOIN pledges
 "
