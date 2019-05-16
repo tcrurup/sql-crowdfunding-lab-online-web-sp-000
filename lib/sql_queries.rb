ab17cb8f9090c09324c82567423c02737ec939b0
Anthony Rurup <tcrurup@gmail.com>
2019-05-16 16:31:04 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -42,7 +42,8 @@ ORDER BY SUM(pledges.amount)"
 end
 
 def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_category
-"Write your SQL query Here"
+"SELECT 
+FROM "
 end
 
 def selects_the_category_name_and_the_sum_total_of_the_all_its_pledges_for_the_books_category
