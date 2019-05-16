324651bc6d261ba55b5c926ddfc664ae5ac13c22
Anthony Rurup <tcrurup@gmail.com>
2019-05-16 16:28:21 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -39,7 +39,6 @@ INNER JOIN pledges
 ON users.id = pledges.user_id
 GROUP BY users.name
 ORDER BY users.name"
-
 end
 
 def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_category
